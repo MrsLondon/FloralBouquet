bfa5e6c70d776f4ed70b84531eaa1da8b653ed24
Maria sophia Mascardo <mariasophiamascardo@Pias-MacBook-Air.local>
2025-01-31 20:17:13 +0100

    navbar 2

--- src/components /Homepage.jsx
+++ src/components /Homepage.jsx
@@ -1,6 +1,8 @@
+import '../style/homepage.css';
+
 function Homepage() {
   return (
-   <div>
+   <div className='homepage'>
     <h2>Flowers</h2>
     <p>"With a gentle smile, they extended the flower, its petals soft and vibrant in the light. A quiet moment passed before the other person accepted it, fingers brushing lightly against the stem—a silent exchange of warmth and meaning."</p>
    </div>
