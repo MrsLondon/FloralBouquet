c0cfea96bd6597a4dd5ef5c8aa6786114099e71a
ayat <abuhajayat@gmail.com>
2025-02-01 22:40:06 +0100

    paes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ function Homepage() {
     <div className="flex flex-col items-center justify-center min-h-screen text-center p-5">
     
     <p className="text-lg max-w-2xl text-gray-700 leading-relaxed mb-6">
-    "With a gentle smile, they extended the flower, its petals soft and vibrant in the light. A quiet moment passed before the other person accepted it, fingers brushing lightly against the stem—a silent exchange of warmth and meaning."
+    "With a gentle smile, they extended the flower, its petals soft and vibrant in the light. A quiet moment passed before the other person accepted it, fingers brushing lightly against the stem—a silent exchange of warmth and meaning.."
     </p>
     <Card />
     </div>
--- src/pages/ValentinePage.jsx
+++ src/pages/ValentinePage.jsx
@@ -2,7 +2,7 @@ const ValentinePage = () => {
     return (
       <div>
         <h1>404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
+        <p>Sorry, the page you are looking for does not exist...</p>
       </div>
     );
   };
