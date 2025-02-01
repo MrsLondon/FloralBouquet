7db9103d5ac53c5a48d13e7570351633f9328e9d
Maria sophia Mascardo <mariasophiamascardo@Pias-MacBook-Air.local>
2025-02-01 14:52:34 +0100

    card fixed

--- src/components /Homepage.jsx
+++ src/components /Homepage.jsx
@@ -1,35 +1,18 @@
 import '../style/homepage.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import Card from '../components /card.jsx';
 
 function Homepage() {
-  const [flowers, setFlowers] = useState([]);
   
-  useEffect(() => {
-    axios.get('https://flowerstore-api-json-server.onrender.com/flowers')
-      .then(response => {
-        setFlowers(response.data);
-      })
-      .catch(error => {
-        console.error('There was an error!', error);
-      });
-  }, []);
+  
+ 
   
   return (
-    <div className='homepage'>
-      <h2>Flowers</h2>
-      <p>"With a gentle smile, they extended the flower, its petals soft and vibrant in the light. A quiet moment passed before the other person accepted it, fingers brushing lightly against the stem—a silent exchange of warmth and meaning."</p>
-      
-      {flowers.map(flower => {
-        return (
-          <div key={flower.id} className='flower'>
-            <h3>{flower.name}</h3>
-            <img src={flower.imageUrl} alt={flower.name} />
-            <p>{flower.description}</p>
-            <p>Price: ${flower.price}</p>
-          </div>
-        );
-      })}
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-5">
+    
+    <p className="text-lg max-w-2xl text-gray-700 leading-relaxed mb-6">
+    "With a gentle smile, they extended the flower, its petals soft and vibrant in the light. A quiet moment passed before the other person accepted it, fingers brushing lightly against the stem—a silent exchange of warmth and meaning."
+    </p>
+    <Card />
     </div>
   );
 }
